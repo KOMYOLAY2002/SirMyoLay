6ff1b54fdc61393c139a6be9d82b29ed6f209209
KOMYOLAY2002 <105275159+KOMYOLAY2002@users.noreply.github.com>
2023-06-03 16:11:18 +0630

    Update minhtet.py

--- minhtet.py
+++ minhtet.py
@@ -6,6 +6,30 @@ green = ('\033[1;32m')
 white = ('\033[1;37m')
 red = ('\033[1;31m')
 
+import getpass
+
+attemp = 0
+
+while attemps < 12345677901:
+
+username  = input(' \033[0;95mEnter Username : ')
+
+if username  == 'MMHtet'
+
+print(' \033[0;97mYou Have Successfully Logged in.')
+
+break
+
+else:
+
+print(' Incorrect username ')
+
+attemps += 1
+
+continue
+
+os.system('clear')
+
 print('<------------------------------------>')
 bit = platform.architecture()[0]
 if bit=='64bit':
